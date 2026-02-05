5596e4b96a0aced3bdb55c534d07ef086c3e7bdd
adedicova <alice.dedicova.s@gyarab.cz>
2026-02-05 18:31:54 +0100

    prevodnik men

--- kurzy/exchange.py
+++ kurzy/exchange.py
@@ -1,7 +1,55 @@
 import httpx
 
 r = httpx.get('https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt')
-print(r.text)
-print(r.status)
+print("server odpovedel", r.status_code)
+
+
 lines = r.text.split('\n')
-print(lines[0])
+print('Kurzy pro den', lines[0].split(' ')[0])
+
+line_eur = ""
+for line in lines:
+    if "EUR" in line:
+        line_eur = line
+        break
+
+rate_str = line_eur.split('|')[-1]
+rate = float(rate_str.replace(',','.'))
+
+print("Kurz euro je", rate)
+
+
+while True:
+    while True:
+        q = input("EUR -> CZK (1) / CZK -> EUR (2), q (quit) ")
+
+        if (q == "q"):
+            exit(0)
+        try: 
+            currency = float(q)
+            break
+        except ValueError:
+            print("Neplatny vstup, zadej 1 nebo 2")
+
+    if(currency == 1):
+        while True:
+            s = input("Kolik mas EUR? ")
+            try:
+                value_in = float(s)
+                break
+            except ValueError:
+                print("Neplatný vstup, zadej číslo.")
+        value_out = value_in * rate
+
+        print (f"Tak to mas {value_out:.02f} CZK")
+    else:
+        while True:
+            s = input("Kolik mas CZK? ")
+            try:
+                value_in = float(s)
+                break
+            except ValueError:
+                print("Neplatný vstup, zadej číslo.")
+        value_out = value_in / rate
+
+        print (f"Tak to mas {value_out:.02f} EUR")
